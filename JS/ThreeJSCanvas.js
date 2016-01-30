524163e3fea806c0de8ba5922b14b25a4e95acff
zajako <zajako@gmail.com>
2016-01-30 03:07:47 -0500

    moved stuff to init

--- JS/ThreeJSCanvas.js
+++ JS/ThreeJSCanvas.js
@@ -77,7 +77,12 @@ THREECanvas.prototype.init = function(){
     this.loadTexture("./IMG/Textures/CobbleFloor.png", 12, 12);
     this.loadTexture("./IMG/Textures/TempleWall.png", 10, 1);
 
-
+    this.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: this.textures[1]}), new THREE.Vector3(0, 0, -25), new THREE.Vector3(0,0,0));
+    this.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: this.textures[1]}), new THREE.Vector3(0, 0, 25), new THREE.Vector3(0,0,0));
+    this.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: this.textures[1]}), new THREE.Vector3(-25, 0, 0), new THREE.Vector3(0,Math.PI / 2,0));
+    this.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: this.textures[1]}), new THREE.Vector3(25, 0, 0), new THREE.Vector3(0,Math.PI / 2,0));
+    this.addGeometry(THREE.BoxGeometry, 50, 50, 0.1, new THREE.MeshBasicMaterial({color: 0xffffff, map: this.textures[0]}), new THREE.Vector3(0, -2, 0), new THREE.Vector3(Math.PI / 2,0,0));
+    this.camera.position.z = 0;
 };
 
 THREECanvas.prototype.addGeometry = function(geoType, width, height, depth, material, position, rotation){
@@ -149,15 +154,6 @@ $("body").keypress(function(event){
             break;
     }
 });
-var myThreeCanvas = new THREECanvas();
-
-myThreeCanvas.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: myThreeCanvas.textures[1]}), new THREE.Vector3(0, 0, -25), new THREE.Vector3(0,0,0));
-myThreeCanvas.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: myThreeCanvas.textures[1]}), new THREE.Vector3(0, 0, 25), new THREE.Vector3(0,0,0));
-myThreeCanvas.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: myThreeCanvas.textures[1]}), new THREE.Vector3(-25, 0, 0), new THREE.Vector3(0,Math.PI / 2,0));
-myThreeCanvas.addGeometry(THREE.BoxGeometry, 50, 5, 1, new THREE.MeshBasicMaterial({color: 0xffffff, map: myThreeCanvas.textures[1]}), new THREE.Vector3(25, 0, 0), new THREE.Vector3(0,Math.PI / 2,0));
-myThreeCanvas.addGeometry(THREE.BoxGeometry, 50, 50, 0.1, new THREE.MeshBasicMaterial({color: 0xffffff, map: myThreeCanvas.textures[0]}), new THREE.Vector3(0, -2, 0), new THREE.Vector3(Math.PI / 2,0,0));
-
-myThreeCanvas.camera.position.z = 0;
 
 function update()
 {
